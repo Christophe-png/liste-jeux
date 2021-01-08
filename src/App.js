b384e35fde03898bd84a4be70c3104244b27fe9e
CASTAN <christophecastan4@gmail.com>
2021-01-08 16:01:48 +0100

    add Game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Game from "./components/Game";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header title="Wild Games" />
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Game.css";
+
+function Game() {
+  return (
+    <article className="Game">
+      <h3>Game title</h3>
+      <img
+        src="https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg"
+        alt="Game Title"
+      />
+      <p>Rating: 4.5</p>
+    </article>
+  );
+}
+
+export default Game;
