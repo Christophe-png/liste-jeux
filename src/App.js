ba524c8481e617aa925e156cf792e183e7e2b724
CASTAN <christophecastan4@gmail.com>
2021-01-08 16:17:49 +0100

    Pass props to Game component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
       <Header title="Wild Games" />
-      <Game />
+      <Game
+        name="Grand Theft Auto V"
+        background_image="https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg"
+        rating={4.47}
+      />
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Game.css";
 
-function Game() {
+function Game(props) {
   return (
     <article className="Game">
-      <h3>Game title</h3>
-      <img
-        src="https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg"
-        alt="Game Title"
-      />
-      <p>Rating: 4.5</p>
+      <h3>{props.name}</h3>
+      <img src={props.background_image} alt="Game Title" />
+      <p>Rating: {props.rating}</p>
     </article>
   );
 }
